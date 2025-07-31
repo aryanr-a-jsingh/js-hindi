3d14cf9637d4d92a2f9d168595215ee229deee70
aryanr-a-jsingh <codethepenguin@gmail.com>
2025-07-31 17:04:13 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144255
+let accountEmail="hitesh@gmail.com"
+var accountPassword="123"
+accocuntCity="Jaipur"
+let accountState;
+//accountId=2 // not allwed 
+
+accountEmail="hc@hc.com"
+accountPassword="anamika"
+accountCity="Bengaluru"
+
+/*
+do not use var 
+because it creates problem in block scope and functional scope 
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
